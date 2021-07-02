da538339912a34aa85f805e02549805f6b58a480
Aji Kamaludin <aji19kamaludin@gmail.com>
2021-07-02 14:32:22 +0700

    restructure with hapi plugin

---
+++ src/api/notes/routes.js
@@ -0,0 +1,34 @@
+const routes = (handler) => [
+  {
+    method: 'GET',
+    path: '/',
+    handler: () => 'Hallo',
+  },
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: handler.postNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: handler.getNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: handler.getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: handler.putNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: handler.deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
