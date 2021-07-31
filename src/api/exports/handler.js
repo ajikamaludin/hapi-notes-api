8b80ae3b2f917df45f0226164b190fd115eb38c6
Aji Kamaludin <aji19kamaludin@gmail.com>
2021-08-01 00:38:31 +0700

    export notes implemented

---
+++ src/api/exports/handler.js
@@ -0,0 +1,32 @@
+class ExportsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postExportNotesHandler = this.postExportNotesHandler.bind(this);
+  }
+
+  async postExportNotesHandler(request, h) {
+    try {
+      this._validator.validateExportNotesPayload(request.payload);
+
+      const message = {
+        userId: request.auth.credentials.id,
+        targetEmail: request.payload.targetEmail,
+      };
+
+      await this._service.sendMessage('export:notes', JSON.stringify(message));
+
+      const response = h.response({
+        status: 'success',
+        message: 'Permintaan Anda dalam antrean',
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      return error;
+    }
+  }
+}
+
+module.exports = ExportsHandler;
